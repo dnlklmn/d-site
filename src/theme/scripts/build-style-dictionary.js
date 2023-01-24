d49d694e01041ff37685043cbd05b04fc35ee3db
dnlklmn <pelicansinberlin@gmail.com>
2023-01-24 13:20:30 +0100

    tokens first steps

---
+++ src/theme/scripts/build-style-dictionary.js
@@ -0,0 +1,79 @@
+const StyleDictionaryPackage = require("style-dictionary");
+
+function createArray({ dictionary }) {
+  const arr = dictionary.allTokens;
+  return JSON.stringify(arr);
+}
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+StyleDictionaryPackage.registerFormat({
+  name: "css/variables",
+  formatter: function(dictionary) {
+    return `${this.selector} {\n${dictionary.allProperties
+      .map((prop) => `  --${prop.name}: ${prop.value};`)
+      .join("\n")}\n}`;
+  },
+});
+
+StyleDictionaryPackage.registerTransform({
+  name: "size/px",
+  type: "value",
+  transitive: true,
+
+  matcher: (token) =>
+    ["fontSizes", "dimension", "borderRadius", "spacing"].includes(token.type),
+  transformer: (token) => parseFloat(token.value) + "px",
+});
+
+function getStyleDictionaryConfig(theme, sourcePath) {
+  return {
+    source: [sourcePath],
+    source: [`tokens/${theme}.json`],
+    include: [`tokens/global.json`],
+    format: {
+      createArray,
+    },
+    platforms: {
+      web: {
+        transformGroup: "css",
+        transforms: ["attribute/cti", "name/cti/kebab", "size/px"],
+        buildPath: "styles/",
+        files: [
+          {
+            destination: `${theme}.css`,
+            format: "css/variables",
+            // selector: `.${theme}-theme`,
+            selector: `:root`,
+            filter: ({ isSource }) => {
+              return isSource;
+            },
+          },
+        ],
+      },
+    },
+  };
+}
+
+console.log("Build started...");
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFERENT BRANDS
+
+const args = process.argv.slice(2);
+const brand = args[0];
+const sourcePath = args[1];
+
+["dark", "light"].map(function(theme) {
+  console.log("\n==============================================");
+  console.log(`\nProcessing: [${theme}]`);
+
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(theme)
+  );
+
+  StyleDictionary.buildPlatform("web");
+
+  console.log("\nEnd processing");
+});
+
+console.log("\n==============================================");
+console.log("\nBuild completed!");
