3b267032351861086d66a5084f86ee295ebab842
dnlklmn <pelicansinberlin@gmail.com>
2023-01-25 13:37:32 +0100

    button css reorg

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,10 @@
 import { motion } from "framer-motion";
-import styled from "styled-components";
+import "../theme/semantic/components.css";
 
-const ButtonBase = styled(motion.button)`
-  width: fit-content;
-  height: auto;
-  background-color: var(--text-contrast);
-  display: flex;
-  justify-content: flex-start;
-  border-radius: 1px;
-  border: none;
-  padding: 16px 16px;
-  cursor: pointer;
-  color: var(--text-match-bg);
-`;
-
-export const Button = ({ onTap, toggleDirection }: any) => {
+export const Button = ({ onTap }: any) => {
   return (
-    <ButtonBase onTap={onTap} className="body-2">
+    <motion.button onTap={onTap} className="button body-2">
       <strong> Get in touch</strong>
-    </ButtonBase>
+    </motion.button>
   );
 };
