c44690b10111c13fe7c2f81bf5bf2f4262d64f07
dnlklmn <pelicansinberlin@gmail.com>
2023-02-10 14:13:01 +0100

    content edit

--- src/components/text-block.tsx
+++ src/components/text-block.tsx
@@ -8,18 +8,17 @@ export default function TextBlock({
   textButton,
 }: any) {
   return (
-    <div className="flex flex-row px-48 gap-12 items-center">
-      <div className="w-full flex flex-col gap-4">
-        <span className="h5">{title}</span>
-        <p className="body-2">{body}</p>
+    <div className="w-full flex flex-col gap-1">
+      <span className="h5">{title}</span>
+      <p className="body-2">{body}</p>
+      {buttonLabel ? (
         <Button
           textButton={textButton}
           label={buttonLabel}
           to="/getintouch"
           iconAfter={<IoArrowForward />}
         />
-      </div>
-      <div className="w-full h-[240px] rounded-sm bg-[color:var(--bg-dip)]" />
+      ) : null}
     </div>
   );
 }
@@ -27,6 +26,5 @@ export default function TextBlock({
 TextBlock.defaultProps = {
   title: "Components in Figma",
   body: "Using properties and variants align the components and make them flexible and easily reusable.",
-  buttonLabel: "Figma file",
   textButton: true,
 };
--- src/pages/DSite.tsx
+++ src/pages/DSite.tsx
@@ -8,6 +8,8 @@ import TokensStudioIcon from "../components/logos/tokens-studio-icon";
 import GithubIcon from "../components/logos/github-icon";
 import TailwindIcon from "../components/logos/tailwind-icon";
 
+import TextBlock from "../components/text-block";
+
 const lightArray = Object.entries(light);
 lightArray.map((category) => {
   console.log(category[0]);
@@ -18,7 +20,7 @@ lightArray.map((category) => {
 
 export function DSite() {
   return (
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col gap-20 overflow-visible">
       <div className="px-36">
         <span className="hero text-[color:var(--fg-contrast)]">
           An interconnected <br /> system demo
@@ -50,6 +52,39 @@ export function DSite() {
             <ReactIcon />
           </div>
         </div>
+      </div>
+      <div className="flex flex-row gap-20 items-center content-start overflow-visible">
+        <div className="w-1/2 flex flex-col gap-4 pl-48 py-2 h-fit">
+          <div className="flex flex-row items-center gap-4">
+            <span className="h5">INTRO</span>
+            <div className="w-[10000px] h-[1px] bg-black" />
+          </div>
+          <TextBlock
+            title="Problem"
+            body="Keeping design and development in sync during the production
+              process is an extensive effort. <br />
+              Production is an iterative process, where designs evolve from
+              initial ideas to wireframes, mockups, and final designs, while a
+              range of setbacks present themselves as development advances,
+              requiring designers to go back to the drawing board and rethink
+              their solutions."
+          />
+          <TextBlock
+            title="Solution"
+            body="Extract shared decisions to facilitate collaboration: build a
+            common ground all parties can access and manipulate, to allow
+            working together on the product, rather than giving instructions
+            to one another."
+          />
+          <TextBlock
+            title="Goal"
+            body="Build a bridge between design and development enabling everyone to participate in the building process."
+          />
+        </div>
+        <img
+          className="h-fill w-1/2"
+          src="https://firebasestorage.googleapis.com/v0/b/d-site-8d30a.appspot.com/o/intro.png?alt=media&token=a8aa05c6-1059-4f38-8d13-566e5db50f40"
+        />
       </div>
       <div className="flex flex-row px-48 gap-12 items-center">
         <div className="w-full flex flex-col gap-4">
