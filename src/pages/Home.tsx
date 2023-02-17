5a05a8d9ed839c585b0f4d7daf80f77800e2c920
dnlklmn <pelicansinberlin@gmail.com>
2023-02-17 14:50:01 +0100

    tokens fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import "../theme/semantic/typography-semantic.css";
 
 import { FloaterArea } from "../components/floater-area";
 import { Button } from "../components/button";
-import { Navbar } from "../components/navbar";
 
 export function Home() {
   return (
@@ -18,7 +17,6 @@ export function Home() {
         url('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,400;0,500;0,600;1,400;1,500&family=Inter:wght@400;500;700&display=swap');
       </style>
       <div className="px-36">
-        <Navbar />
         <div className="flex flex-col gap-6">
           <span className="hero text-[color:var(--fg-contrast)]">
             I design and build <br /> user interfaces
