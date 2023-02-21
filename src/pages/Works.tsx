b58e1b60d85c21fed76eefddcdb29bd3bf362b5e
dnlklmn <pelicansinberlin@gmail.com>
2023-02-21 13:46:56 +0100

    works works

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -51,20 +51,27 @@ export function Works() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 flex-wrap">
           <Link className="w-full h-[240px]" to="/works/this-site">
             <div className="group h-full bg-cover rounded-md overflow-clip shadow-md hover:shadow-lg bg-[url('https://firebasestorage.googleapis.com/v0/b/d-site-8d30a.appspot.com/o/this-site.png?alt=media&token=1245c8cd-601c-4069-a042-2807633524ab')]">
-              <div className="flex flex-col w-full   h-full p-4 justify-end backdrop-blur-sm group-hover:backdrop-blur-none transition-all">
-                <p className="h5 text-[color:var(--fg-always-dark)] opacity-100 group-hover:opacity-0 transition-opacity">
+              <div className="flex flex-col w-full text-[color:var(--fg-always-dark)] h-full p-4 justify-end backdrop-blur-sm group-hover:backdrop-blur-none transition-all">
+                <p className="h5 opacity-100 group-hover:opacity-0 transition-opacity">
                   <i>This Site</i>
+                </p>
+                <p className="body-2 group-hover:opacity-0 transition-opacity">
+                  Design System
                 </p>
               </div>
             </div>
           </Link>
           <Link className="w-full h-[240px]" to="/works/this-site">
-            <div className="flex flex-col w-full shadow-md rounded-md overflow-clip h-full bg-white p-4 justify-end ">
-              <p className="h5 text-[color:var(--fg-always-dark)]">
-                <i>Web3 Address Generator</i>
-              </p>
+            <div className="group h-full bg-cover rounded-md overflow-clip shadow-md hover:shadow-lg bg-[url('https://firebasestorage.googleapis.com/v0/b/d-site-8d30a.appspot.com/o/address-generator.png?alt=media&token=c8301039-696d-44fc-89cd-ba80c7ec7f80')]">
+              <div className="flex flex-col w-full   h-full p-4 justify-end backdrop-blur-sm group-hover:backdrop-blur-none transition-all">
+                <p className="h5 text-[color:var(--fg-always-dark)] opacity-100 group-hover:opacity-0 transition-opacity">
+                  <i>Web3 Address Generator</i>
+                </p>
+                <p className="body-2">Figma Plugin</p>
+              </div>
             </div>
           </Link>
+
           <Link className="w-full h-[240px]" to="/works/this-site">
             <div className="flex flex-col w-full shadow-md rounded-md overflow-clip h-full bg-white p-4 justify-end">
               <p className="h5 text-[color:var(--fg-always-dark)]">
